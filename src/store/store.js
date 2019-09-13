4824bf97c250dfa5c60a465910f942b0f535d058
TomaszSiniak <siniak.tomasz@gmail.com>
2019-09-13 17:22:05 +0200

    bugfix redux extension

--- src/store/store.js
+++ src/store/store.js
@@ -5,7 +5,7 @@ import thunk from 'redux-thunk';
 
 const store = createStore(
   mainReducer,
-  compose(applyMiddleware(thunk),    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() || compose))
+  compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__() || compose)
 );
 
 export default store;
